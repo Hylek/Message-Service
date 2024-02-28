9789e0208e8bf091521eced6923c4ec8184016bc
Daniel Cumbor <daniel@lettheinsideout.com>
2024-02-28 20:54:56 +0000

    Created Message Service package.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Message Service
+
+The Message Service is a package that implements TinyMessenger into Unity and allows it to be used with MonoBehaviours.
---
+++ Runtime/ExtendedBehaviour.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DC.ServiceLocator;
+using UnityEngine;
+
+namespace DC.MessageService
+{
+    public class ExtendedBehaviour : MonoBehaviour
+    {
+        protected ITinyMessengerHub EventHub => _eventHub ??= Locator.Find<ITinyMessengerHub>();
+
+        private Dictionary<Type, TinyMessageSubscriptionToken> _tokens;
+        private ITinyMessengerHub _eventHub;
+
+        protected virtual void Awake()
+        {
+            _tokens = new Dictionary<Type, TinyMessageSubscriptionToken>();
+        }
+    
+        protected void Subscribe<T>(Action<T> action) where T : class, ITinyMessage
+        {
+            if (!Locator.DoesServiceExist(typeof(ITinyMessengerHub)))
+            {
+                Locator.AddNewService<ITinyMessengerHub>(new TinyMessengerHub());
+            }
+            
+            _tokens.Add(typeof(T), EventHub.Subscribe(action));
+        }
+        
+        protected void Unsubscribe<T>()
+        {
+            var type = typeof(T);
+            
+            foreach (var token in
+                     _tokens.Where(token => token.Key == type))
+            {
+                Debug.Log($"{gameObject.name} has unsubscribed from message type {token.Key.Name}");
+                EventHub.Unsubscribe(token.Value);
+                _tokens.Remove(token.Key);
+
+                break;
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (_tokens.Count <= 0) return;
+            
+            Debug.Log($"{gameObject.name} is cleaning up it's messages.");
+            
+            foreach (var token in _tokens)
+            {
+                EventHub.Unsubscribe(token.Value);
+            }
+            _tokens.Clear();
+        }
+    }
+}
